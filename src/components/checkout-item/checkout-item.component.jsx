ddc862bef33b1bf0a8a4522f33294ffe67f44ac8
Kurtis Hastie <kurtis.hastie@gmail.com>
2021-06-19 08:49:01 -0400

    Updated and refactored code. Created selectors and updated corresponding code

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
-import { addItem, clearItemFromCart, removeItem } from '../../redux/cart/cart.actions';
+import { addItem, clearItemFromCart, removeItem  } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
